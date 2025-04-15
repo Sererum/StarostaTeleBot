a528987b5946fc72ed574440f458194e6f0951ee
Sererum <filippovgleb.05@yandex.ru>
2025-04-15 20:53:07 +0300

    Инициализация базового бота, отвечающего двумя способами, пока без логгера

---
+++ src/main/java/com/sererum/StarostaTeleBot/StarostaTeleBotApplication.java
@@ -0,0 +1,13 @@
+package com.sererum.StarostaTeleBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarostaTeleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarostaTeleBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sererum/StarostaTeleBot/StarostaTeleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sererum.StarostaTeleBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarostaTeleBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
